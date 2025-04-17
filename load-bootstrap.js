341babdd3253e313132507bc08125f90412fe6af
Sarayuth15 <rayuthek430@gmail.com>
2025-04-17 14:01:32 +0900

    add

---
+++ load-bootstrap.js
@@ -0,0 +1,4 @@
+const link = document.createElement('link');
+link.rel = 'stylesheet'
+link.href = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css';
+document.head.appendChild(link);
